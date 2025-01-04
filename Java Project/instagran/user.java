2ba03553bdf824cec9b2edd8d22c079f4657b507
Rohit Nathe <rohitnathe077@gmail.com>
2025-01-04 20:35:39 +0530

    Create user.java

---
+++ Java Project/instagran/user.java
@@ -0,0 +1,14 @@
+package com.Instagram.Main;
+
+public class User extends Mylogic {
+	 
+	    String username;
+	    String password;
+
+	    User(String username, String password) {
+	        this.username = username;
+	        this.password = password;
+	    }
+	
+
+}
